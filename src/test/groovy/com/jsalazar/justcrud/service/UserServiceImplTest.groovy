c110b5c2369d755141c128c8829790b8f83c1384
jsalazare <javi_9315@hotmail.com>
2020-09-17 21:05:52 -0700

    add first test class 2

---
+++ src/test/groovy/com/jsalazar/justcrud/service/UserServiceImplTest.groovy
@@ -0,0 +1,22 @@
+package com.jsalazar.justcrud.service
+
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.powermock.core.classloader.annotations.PrepareForTest
+import org.powermock.modules.junit4.PowerMockRunner
+
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ UserServiceImpl.class })
+class UserServiceImplTest {
+
+    @Before
+    void before () {
+
+    }
+
+    @Test
+    void test() {
+    }
+}
